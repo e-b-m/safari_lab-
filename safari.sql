02c4716a404a3bed486a162ba7335b6712f96520
Emma Bayley-Melendez <e.melendez98@outlook.com>
2022-12-01 15:26:45 +0000

    Emma added files to GitHub

---
+++ safari.sql
@@ -0,0 +1,12 @@
+
+DROP TABLE animals; 
+
+CREATE TABLE animals (
+id SERIAL PRIMARY KEY, 
+name VARCHAR(255),
+type VARCHAR(255), 
+age INT, 
+enclosure_id INT
+); 
+
+INSERT INTO animals() VALUES (); 
